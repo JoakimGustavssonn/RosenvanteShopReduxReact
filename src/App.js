33d7038e7b53c15d7b1801e07334d1b25f98a2f9
JoakimGustavssonn <joakimgustavsson1993@hotmail.com>
2020-11-13 23:14:57 +0100

    Applied ShowStore component clean code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Products from './components/Products';
-import Basket from './components/Basket';
-import {Provider} from 'react-redux';
-import store from './store';
-import Footer from './components/Footer';
+import ShowStore from './components/ShowStore';
 
-
-import {Container, Row, Col} from 'react-bootstrap';
 
 
 class App extends Component{
@@ -17,38 +11,8 @@ class App extends Component{
   render () {
     return (
       
-      <Provider store={store}> 
+    <ShowStore />
       
-      <Container>
-        
-          <div id ="stickyHeader" >
-          <h1 > Rosenvante Butik</h1>
-          <hr/>
-          </div>
-         
-      
-
-        <Row >
-          <Col className="pb-4" md={8} sm={12} xs={12}>     
-              <Products />
-
-          </Col>
-          
-          
-
-          <Col sm={4} md={4} xs={4}>
-              <Basket />
-          </Col> 
-
-        </Row> 
-
-        <Footer />
-                
-        </Container>
-        
-
-        </Provider>
-
     );
     
   }
